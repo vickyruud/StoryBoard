61cc52d7ba86ce16f95404f502eee3f01d43296b
JhanviShah1 <jhanvi.213@gmail.com>
2021-10-20 15:35:52 -0400

    added data to users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,1 +1,45 @@
 -- Users table seeds here (Example)
+INSERT INTO users (
+    name, email, password,privilege)
+    VALUES (
+    'Jennie Powers', 'lilabell@live.com', 'password',1);
+    INSERT INTO users (
+    name, email, password,privilege)
+    VALUES (
+    'Maggie Norris', 'miafletcher@inbox.com', 'password',1);
+    INSERT INTO users (
+    name, email, password,privilege)
+    VALUES (
+    'Justin Reyes', 'austindotson@aol.com', 'password',1);
+    INSERT INTO users (
+    name, email, password,privilege)
+    VALUES (
+    'Effie Phillips', 'evelynaustin@ymail.com', 'password',1);
+    INSERT INTO users (
+    name, email, password,privilege)
+    VALUES (
+    'Travis Mendoza', 'lucypittman@gmx.com', 'password',2);
+    INSERT INTO users (
+    name, email, password,privilege)
+    VALUES (
+    'Edgar Harrison', 'keiragates@ymail.com', 'password',2);
+    INSERT INTO users (
+    name, email, password,privilege)
+    VALUES (
+    'Delia Caldwell', 'elliotbaker@yahoo.com', 'password',2);
+    INSERT INTO users (
+    name, email, password,privilege)
+    VALUES (
+    'Calvin Thornton', 'rileyrichard@outlook.com', 'password',2);
+    INSERT INTO users (
+    name, email, password,privilege)
+    VALUES (
+    'Elizabeth Lane', 'henryhaynes@gmx.com', 'password',2);
+    INSERT INTO users (
+    name, email, password,privilege)
+    VALUES (
+    'James Banks', 'sydneylopez@live.com', 'password',2);
+    INSERT INTO users (
+    name, email, password,privilege)
+    VALUES (
+    'Micheal Page', 'kennedyparks@mail.com', 'password',1);
