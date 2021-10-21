ec5b41e127af6820ac23ce2641e2550a2ff57120
vickyruud <aprajit.k@gmail.com>
2021-10-21 11:16:10 -0400

    remove privileges

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -9,8 +9,7 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  privilege INTEGER NOT NULL
+  password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE stories (
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,45 +1,45 @@
 -- Users table seeds here (Example)
 INSERT INTO users (
-    name, email, password,privilege)
+    name, email, password)
     VALUES (
-    'Jennie Powers', 'lilabell@live.com', 'password',1);
+    'Jennie Powers', 'lilabell@live.com', 'password');
     INSERT INTO users (
-    name, email, password,privilege)
+    name, email, password)
     VALUES (
-    'Maggie Norris', 'miafletcher@inbox.com', 'password',1);
+    'Maggie Norris', 'miafletcher@inbox.com', 'password');
     INSERT INTO users (
-    name, email, password,privilege)
+    name, email, password)
     VALUES (
-    'Justin Reyes', 'austindotson@aol.com', 'password',1);
+    'Justin Reyes', 'austindotson@aol.com', 'password');
     INSERT INTO users (
-    name, email, password,privilege)
+    name, email, password)
     VALUES (
-    'Effie Phillips', 'evelynaustin@ymail.com', 'password',1);
+    'Effie Phillips', 'evelynaustin@ymail.com', 'password');
     INSERT INTO users (
-    name, email, password,privilege)
+    name, email, password)
     VALUES (
-    'Travis Mendoza', 'lucypittman@gmx.com', 'password',2);
+    'Travis Mendoza', 'lucypittman@gmx.com', 'password');
     INSERT INTO users (
-    name, email, password,privilege)
+    name, email, password)
     VALUES (
-    'Edgar Harrison', 'keiragates@ymail.com', 'password',2);
+    'Edgar Harrison', 'keiragates@ymail.com', 'password');
     INSERT INTO users (
-    name, email, password,privilege)
+    name, email, password)
     VALUES (
-    'Delia Caldwell', 'elliotbaker@yahoo.com', 'password',2);
+    'Delia Caldwell', 'elliotbaker@yahoo.com', 'password');
     INSERT INTO users (
-    name, email, password,privilege)
+    name, email, password)
     VALUES (
-    'Calvin Thornton', 'rileyrichard@outlook.com', 'password',2);
+    'Calvin Thornton', 'rileyrichard@outlook.com', 'password');
     INSERT INTO users (
-    name, email, password,privilege)
+    name, email, password)
     VALUES (
-    'Elizabeth Lane', 'henryhaynes@gmx.com', 'password',2);
+    'Elizabeth Lane', 'henryhaynes@gmx.com', 'password');
     INSERT INTO users (
-    name, email, password,privilege)
+    name, email, password)
     VALUES (
-    'James Banks', 'sydneylopez@live.com', 'password',2);
+    'James Banks', 'sydneylopez@live.com', 'password');
     INSERT INTO users (
-    name, email, password,privilege)
+    name, email, password)
     VALUES (
-    'Micheal Page', 'kennedyparks@mail.com', 'password',1);
+    'Micheal Page', 'kennedyparks@mail.com', 'password');
