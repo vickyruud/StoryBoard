4b1dbfb061d4e14091664a0b04bdef80fa5ec365
JhanviShah1 <jhanvi.213@gmail.com>
2021-10-23 15:28:13 -0400

    test

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -13,19 +13,19 @@ module.exports = (db) => {
 
   //renders the login page
   router.get("/", (req, res) => {
-  const templateVars = {user: undefined};
+  const templateVars = {user:  undefined};
    res.render('login');
   });
 
   //fetches the user from the database based on the email
   const login = function (email, password) {
-    return database.getUserWithEmail(email).then((user) => {   
+    return database.getUserWithEmail(email).then((user) => {
       if (email === user.email && password === user.password) {
         return user;
       } else {
         return null
       }
-      
+
     });
   };
   exports.login = login;
@@ -33,7 +33,7 @@ module.exports = (db) => {
   //the post function calls the login function above and checks if user exists
   // if the user exists then it renders the page else gives error
   router.post('/', (req, res) => {
-    const {email, password} = req.body; 
+    const {email, password} = req.body;
     login(email, password).then(user => {
       if (user) {
         req.session.userId = user.id;
@@ -41,7 +41,7 @@ module.exports = (db) => {
         templateVars = {user: user};
         res.redirect('/');
         } else {
-        res.send("Incorrect username/password"); 
+        res.send("Incorrect username/password");
       }
 
     })
