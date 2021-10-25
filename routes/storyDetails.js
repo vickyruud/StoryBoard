b8461eda6539ac2ca91fec1df7766db6d1652614
Anh Tong <tongkimanh@yahoo.com>
2021-10-25 18:26:25 +0000

    Remove redundant route files

--- routes/storyDetails.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-
-module.exports = (db) => {
-  router.post("/", (req, res) => {
-    console.log("View story details");
-  })
-  return router;
-};
