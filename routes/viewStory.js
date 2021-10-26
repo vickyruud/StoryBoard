7d14141798f2d076966daea35aafb5c35e1bd1ff
Anh Tong <tongkimanh@yahoo.com>
2021-10-26 18:17:06 +0000

    Synch with master

--- routes/viewStory.js
+++ routes/viewStory.js
@@ -11,7 +11,7 @@ const database = require('../database');
 
 
 module.exports = (db) => {
-  
+
 
   router.get("/:id", (req, res) => {
     const storyId = req.params.id;
@@ -19,15 +19,9 @@ module.exports = (db) => {
     const user = req.session.userId;
     return database.getStory(storyId)
     .then(story => {
-<<<<<<< HEAD
-      
-      const templateVars = {story, user};
-        res.render('storyView', templateVars);
-=======
       console.log("This is our story---",story);
         const templateVars = {story, user};
         res.render('storyView', templateVars)
->>>>>>> 3366dd4d340fcfabd2f478b92bc1207f4b872d86
       })
       .catch((error) => {
         console.log(error.message);
