0c4136476d02b786e60a8b34971c47c7e9344e65
vickyruud <aprajit.k@gmail.com>
2021-10-26 12:42:24 -0400

    fix merging issues

--- routes/viewStory.js
+++ routes/viewStory.js
@@ -9,15 +9,18 @@ const express = require('express');
 const router  = express.Router();
 const database = require('../database');
 
+
 module.exports = (db) => {
+  
+
   router.get("/:id", (req, res) => {
     const storyId = req.params.id;
     const user = req.session.userId;
-    console.log('this is from router : ',user)
     return database.getStory(storyId)
     .then(story => {
-        const templateVars = {story, user};
-        res.render('storyView', templateVars)
+      
+      const templateVars = {story, user};
+        res.render('storyView', templateVars);
       })
       .catch((error) => {
         console.log(error.message);
