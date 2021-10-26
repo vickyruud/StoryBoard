80d8ae43298502e2a163e416cb1a15e4388fa55c
vickyruud <aprajit.k@gmail.com>
2021-10-26 18:42:01 -0400

    fix merging issue

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,22 @@ const clearArea = function(){
   document.getElementById('contents').value = "";
   document.getElementById('title').value = "";
 }
+
+const showContributionBox = function () {
+  const contributionBox = $('#your-contribution');
+  contributionBox.slideToggle('fast');
+}
+
 $(document).ready(() => {
   $("#cbtn").click(function (e) {
     event.preventDefault();
     clearArea();
 
   });
+
+  // $("#add-contribution").click(function (e) {
+  //   event.preventDefault();
+  //   showContributionBox();
+
+  // });
 });
