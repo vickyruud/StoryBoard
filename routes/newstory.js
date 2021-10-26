f6e1462f5a64e97aea09e9f02fa3b03cf1fa8fe0
vickyruud <aprajit.k@gmail.com>
2021-10-26 18:47:32 -0400

    fix newstory.js

--- routes/newstory.js
+++ routes/newstory.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const database = require("../database");
-
 module.exports = (db) => {
   //renders the newstory page
   let templateVars = {};
@@ -11,6 +10,9 @@ module.exports = (db) => {
     res.render("newstory", templateVars);
   });
   router.post("/", (req, res) => {
+    if (req.body.contents === "" || req.body.title === "") {
+      return res.redirect('back');
+    }
     const userId = req.session.userId;
     database
       .insertNewStory({ ...req.body, author_id: userId.id })
@@ -19,6 +21,5 @@ module.exports = (db) => {
       })
       .catch((e) => res.send(e));
   });
-
   return router;
 };
